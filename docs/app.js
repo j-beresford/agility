718f207ee20bb4b2480824083c09f15c891032bc
JustinTBeresofrd@gmail.com <j-beresford9>
2022-03-22 20:20:56 +0000

    footer

--- docs/app.js
+++ docs/app.js
@@ -1,7 +1,8 @@
 const footerTop = document.getElementById('footTop');
 
 footerTop.style.position = 'relative';
-footerTop.style.top = '150px';
+footerTop.style.top = '10px';
+footerTop.style.color = "red"
 
 console.log(footerTop.style.top)
 
